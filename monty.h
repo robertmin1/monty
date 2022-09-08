f9549192fbd23f81f53e6c574040d7e0726caa47
Nganga <eliud.gachunji@gmail.com>
2022-09-08 12:44:25 -0700

    editing the header file

--- monty.h
+++ monty.h
@@ -12,7 +12,9 @@
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
+ *
  * @prev: points to the previous element of the stack (or queue)
+ *
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
